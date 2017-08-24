ca408859ed4db9761d1adb6a972bf3853c076583
nnattawat <armmer1@gmail.com>
2017-08-24 16:16:50 +1000

    Support for change name

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -7,10 +7,28 @@ let defaultState = {
 };
 
 let reducer = (state = defaultState, action) => {
-  return state;
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      }
+      break;
+    default:
+      return state;
+  }
 };
 
 let store = redux.createStore(reducer);
 
-let currentState = store.getState();
-console.log(currentState);
+console.log(`init state:`);
+console.log(store.getState());
+
+let action = {
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'todo'
+}
+store.dispatch(action);
+
+console.log(`state after dispatch CHANGE_SEARCH_TEXT:`);
+console.log(store.getState());
